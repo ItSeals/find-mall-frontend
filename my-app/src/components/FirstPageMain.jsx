3445fed6cf11349fb143017d377f8c11ce4f076e
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2022-12-11 22:52:33 +0200

    add small logik to malls main page. Now is working create and delete buttons. Its alpha version of main mall page

--- my-app/src/components/FirstPageMain.jsx
+++ my-app/src/components/FirstPageMain.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-import FirstPageMainItem from './FirstPageMainItem'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import FirstPageMainItem from './FirstPageMainItem';
 
 const FirstPageMain = (props) => {
+  const [malls, setMalls] = useState([])
+
+  const apiEndPoint = "http://localhost:3000/mall";
+
+  useEffect(() => {
+    const getMalls = async () => {
+      const { data: res } = await axios.get(apiEndPoint)
+      setMalls(res)
+    }
+    getMalls()
+  }, [malls])
+
+  const AddMall = async () => {
+    const mall = {title: `New Mall №${malls.length + 1}`, description: "New description", status: 1 };
+    await axios.post(apiEndPoint, mall);
+    setMalls([...malls, mall]);
+  };
+
+  const MallDelete = async (mall) => {
+    await axios.delete(apiEndPoint + "/" + mall.id);
+    setMalls(malls.filter((p) => p.id !== mall.id));
+  };
+
   return (
     <div {...props} style={{textAlign: 'center'}}>
       <table className='admin' style={{width: '100%'}}>
@@ -17,12 +42,18 @@ const FirstPageMain = (props) => {
           </th>
           <th>
             <div>
-              <button className='btn btn-primary'>Create</button>
+              <button 
+                onClick={AddMall} 
+                className='btn btn-primary'
+              >
+                Create
+              </button>
             </div>
           </th>
         </tr>
-        <FirstPageMainItem/>
-        <FirstPageMainItem/>
+        {malls.map((mall) => (
+          <FirstPageMainItem mall={mall} MD={MallDelete} key={mall.id}/>
+        ))}
       </table>
     </div>
   )
--- my-app/src/components/FirstPageMainItem.jsx
+++ my-app/src/components/FirstPageMainItem.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
+import { MallDelete } from "./FirstPageMain";
 
-const FirstPageMainItem = () => {
+const FirstPageMainItem = ({mall, MD}) => {
+  
   return (
     <tr>
       <td>
-        <div>dfdfdf</div>
+        <div>{mall.title ? mall.title : "Не визначено"}</div>
       </td>
       <td>
-        <div>dfdffgfdf</div>
+        <div>{mall.category ? mall.category : "Не визначено"}</div>
       </td>
       <td>
-        <div>dfdssfdf</div>
+        <div>{mall.location ? mall.location : "Не визначено"}</div>
       </td>
       <td>
         <div>
           <button className='btn btn-primary'>Edit</button>
+        </div>
+        <div>
+          <button
+            onClick={() => MD(mall)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
         </div>
       </td>
     </tr>
