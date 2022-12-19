3e0888cced6ea34e8c3440d5f070a16b13f73d65
hhelgii <opruska2004@gmail.com>
2022-12-19 17:48:28 +0200

    adding authorization

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,19 +4,18 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Malls from './components/Malls';
 import SideBar from './components/SideBar';
 import AllSAndC from './components/AllSAndC';
-
+import './App.css';
+import Login from "./Login";
 function App() {
+  const [success, setSuccess] = useState(false);
   const [activePage, setActivePage] = useState(1)
 
-  if (activePage == 2) {
+  if (success == false) {
     return (
-      <div className= 'container-fluid'>
-        <div className ='row'>
-          <SideBar className='col-2 min-vh-100' sAP={setActivePage}/>
-          <AllSAndC className='col gx-0 text-center'/>
-        </div>
-      </div>
-    )
+      <main className="App">
+        <Login AP={setActivePage} success={success}/>
+      </main>
+    );
   } else {
     return(
       <div className= 'container-fluid'>
