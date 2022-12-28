4764179b3b31bac186fa371bbe51ca9643e56323
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2022-12-28 17:35:16 +0200

    delete useless imports from app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Malls from './components/Malls';
 import SideBar from './components/SideBar';
-import AllSAndC from './components/AllSAndC';
-import AllSAndC_Stores from './components/AllSAndC_Stores';
-import AllSAndC_Rest from './components/AllSAndC_Rest';
 import './App.css';
 import Login from "./Login";
 
 function App() {
   const [success, setSuccess] = useState(true);
 
-  if (success == false) {
+  if (success === false) {
     return (
       <main className="App">
         <Login success={success} setSuccess={setSuccess}/>
