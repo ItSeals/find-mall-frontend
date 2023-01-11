732c5f67b976e56f27ed984919cc254f7c4644a6
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2023-01-11 14:50:08 +0200

    clear AllSAndC_categories

--- my-app/src/components/AllSAndC_SAndE.jsx
+++ my-app/src/components/AllSAndC_SAndE.jsx
@@ -1,111 +1,127 @@
-import axios from 'axios';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { global, networkCall } from ".././helpers/helpers";
-import Dialog from './Dialog';
-import SorCCreate from './allSAndC/SorCCreate';
-import SorCEdit from './allSAndC/SorCEdit';
-import SorCItem from './allSAndC/SorCItem';
+import Dialog from "./Dialog";
+import SorCCreate from "./allSAndC/SorCCreate";
+import SorCEdit from "./allSAndC/SorCEdit";
+import SorCItem from "./allSAndC/SorCItem";
+import SorCItemBG from "./allSAndC/SorCItemBG";
 
 const AllSAndC_SAndE = (props) => {
-  const [malls, setMalls] = useState([])
-  const [createPage, setCreatePage] = useState(false)
-  const [editPage, setEditPage] = useState({is: false, mall: {}})
+  const [allSAndC, setAllSAndC] = useState([]);
+  const [createPage, setCreatePage] = useState(false);
+  const [editPage, setEditPage] = useState(false);
   const [dialog, setDialog] = useState({
     message: "",
     isLoading: false,
-    //Update
-    nameProduct: ""
+    nameProduct: "",
   });
+
   const idMallRef = useRef();
-  const apiEndPoint = `${global.api}/item`;
 
-  const getMalls = async () => {
-    const { data: res } = await axios.get(apiEndPoint)
-    let value = []
-    for (let index = 0; index < res.length; index++) {
-      if (res[index].category.title === "Services & Entertainment") {
-        value.push(res[index])
-      }
-    }
-    setMalls(value)
-  }
-  
   useEffect(() => {
-    getMalls()
-  }, [])
+    updateAllSAndC();
+  }, []);
 
-  const AddMall = async (mall) => {
-    await axios.post(apiEndPoint, mall);
-    setMalls([...malls, mall]);
-    setCreatePage(false)
-    getMalls()
-  };
+  function updateAllSAndC() {
+    networkCall(
+      { url: `${global.api}/item`, type: "get" },
+      (res) => {
+        let value = [];
+        for (let index = 0; index < res.length; index++) {
+          if (res[index].category.title === "Services & Entertainment") {
+            value.push(res[index]);
+          }
+        }
+        setAllSAndC(value);
+      },
+      (error) => console.log("error", error)
+    );
+  }
 
-  const EditMall = async (mall) => {
-    console.log(mall)
-    await axios.put(apiEndPoint + "/" + mall.id, mall);
-    const mallsClone = [...malls];
-    const index = mallsClone.indexOf(mall);
-    mallsClone[index] = { ...mall };
-    setMalls(mallsClone);
-    setEditPage({mall: {}, is: false})
-    getMalls()
-  };
+  function AddSOrC(mall) {
+    networkCall(
+      { url: `${global.api}/item`, type: "post", content: mall },
+      () => updateAllSAndC(),
+      (error) => console.log("error", error)
+    );
+    setCreatePage(false);
+  }
 
-  const handleDialog = (message, isLoading, nameProduct) => {
+  function EditSOrC(mall) {
+    networkCall(
+      { url: `${global.api}/item/${mall.id}`, type: "put", content: mall },
+      () => updateAllSAndC(),
+      (error) => console.log("error", error)
+    );
+    setEditPage(false);
+  }
+
+  function handleDialog(message, isLoading, nameProduct) {
     setDialog({
       message,
       isLoading,
-      //Update
-      nameProduct
+      nameProduct,
     });
-  };
+  }
 
-  const handleDelete = (id) => {
-    //Update
-    const index = malls.findIndex((m) => m.id === id);
-
-    handleDialog("Are you sure you want to delete?", true, malls[index].title);
+  function handleDelete(id) {
+    const index = allSAndC.findIndex((m) => m.id === id);
+    handleDialog(
+      "Are you sure you want to delete?",
+      true,
+      allSAndC[index].title
+    );
     idMallRef.current = id;
-  };
+  }
 
-  const areUSureDelete = async (choose) => {
+  function areUSureDelete(choose) {
     if (choose) {
-      await axios.delete(apiEndPoint + "/" + idMallRef.current);
-      setMalls(malls.filter((m) => m.id !== idMallRef.current));
-      handleDialog("", false);
-    } else {
-      handleDialog("", false);
+      networkCall(
+        { url: `${global.api}/item/${idMallRef.current}`, type: "delete" },
+        () => updateAllSAndC(),
+        (error) => console.log("error", error)
+      );
     }
-  };
+    handleDialog("", false);
+  }
 
   if (createPage) {
     return (
-      <SorCCreate className={`${props.className} position-fixed`} style={{width: '100%', left: '0'}} apiEndPoint={apiEndPoint} AM={AddMall} prePage={setCreatePage}/>
-    )
-  } else if (editPage.is) {
+      <SorCCreate
+        className={`${props.className} position-absolute`}
+        style={{ width: "100%", left: "0" }}
+        AddSOrC={AddSOrC}
+        setCreatePage={setCreatePage}
+      />
+    );
+  } else if (editPage) {
     return (
-      <SorCEdit className={`${props.className} position-fixed`} style={{width: '100%', left: '0'}} ME={EditMall} prePage={setEditPage} mall={editPage.mall}/>
-    )
+      <SorCEdit
+        className={`${props.className} position-absolute`}
+        style={{ width: "100%", left: "0" }}
+        EditSOrC={EditSOrC}
+        setEditPage={setEditPage}
+      />
+    );
   } else {
     return (
-      <div className={`${props.className} position-relative overflow-auto`} style={{height: '100vh'}}>
-        <table className='admin-table position-absolute' style={{width: '100%'}}>
+      <div
+        className={`${props.className} position-relative overflow-auto`}
+        style={{ height: "100vh" }}
+      >
+        <table
+          className="admin-table position-absolute"
+          style={{ width: "100%" }}
+        >
           <thead>
             <tr>
-              <th>
-                Name
-              </th>
-              <th>
-                Category
-              </th>
-              <th>
-                Mall List
-              </th>
-              <th style={{width: '230px', maxWidth: '20%'}}>
-                <button 
+              <th>Name</th>
+              <th>Category</th>
+              <th>Mall List</th>
+              <th style={{ width: "230px", maxWidth: "20%" }}>
+                <button
                   onClick={() => setCreatePage(true)}
-                  className='btn pt-1px'
+                  className="btn pt-1px"
                 >
                   Create
                 </button>
@@ -113,34 +129,28 @@ const AllSAndC_SAndE = (props) => {
             </tr>
           </thead>
           <tbody>
-            <tr><td>{malls[0] !== undefined ? malls[0].title : ''}</td><td>{malls[0] !== undefined ? malls[0].category.title : ''}</td><td>{malls[0] !== undefined ? malls[0].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[1] !== undefined ? malls[1].title : ''}</td><td>{malls[1] !== undefined ? malls[1].category.title : ''}</td><td>{malls[1] !== undefined ? malls[1].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[2] !== undefined ? malls[2].title : ''}</td><td>{malls[2] !== undefined ? malls[2].category.title : ''}</td><td>{malls[2] !== undefined ? malls[2].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[3] !== undefined ? malls[3].title : ''}</td><td>{malls[3] !== undefined ? malls[3].category.title : ''}</td><td>{malls[3] !== undefined ? malls[3].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[4] !== undefined ? malls[4].title : ''}</td><td>{malls[4] !== undefined ? malls[4].category.title : ''}</td><td>{malls[4] !== undefined ? malls[4].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[5] !== undefined ? malls[5].title : ''}</td><td>{malls[5] !== undefined ? malls[5].category.title : ''}</td><td>{malls[5] !== undefined ? malls[5].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[6] !== undefined ? malls[6].title : ''}</td><td>{malls[6] !== undefined ? malls[6].category.title : ''}</td><td>{malls[6] !== undefined ? malls[6].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[7] !== undefined ? malls[7].title : ''}</td><td>{malls[7] !== undefined ? malls[7].category.title : ''}</td><td>{malls[7] !== undefined ? malls[7].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[8] !== undefined ? malls[8].title : ''}</td><td>{malls[8] !== undefined ? malls[8].category.title : ''}</td><td>{malls[8] !== undefined ? malls[8].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
-            <tr><td>{malls[9] !== undefined ? malls[9].title : ''}</td><td>{malls[9] !== undefined ? malls[9].category.title : ''}</td><td>{malls[9] !== undefined ? malls[9].malls.map((mall, index) => { let sep = ''; if (index !== 0) {sep = ', '} return `${sep}${mall.title}` }) : ''}</td><td><button className='btn pt-1px opacity-0'></button></td></tr>
+            <SorCItemBG SorC={allSAndC[0]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[1]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[2]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[3]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[4]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[5]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[6]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[7]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[8]}></SorCItemBG>
+            <SorCItemBG SorC={allSAndC[9]}></SorCItemBG>
           </tbody>
         </table>
-        <table className='admin-table' style={{width: '100%'}}>
+        <table className="admin-table" style={{ width: "100%" }}>
           <thead>
             <tr>
-              <th>
-                Name
-              </th>
-              <th>
-                Category
-              </th>
-              <th>
-                Mall List
-              </th>
-              <th style={{width: '230px', maxWidth: '20%'}}>
-                <button 
+              <th>Name</th>
+              <th>Category</th>
+              <th>Mall List</th>
+              <th style={{ width: "230px", maxWidth: "20%" }}>
+                <button
                   onClick={() => setCreatePage(true)}
-                  className='btn pt-1px'
+                  className="btn pt-1px"
                 >
                   Create
                 </button>
@@ -148,22 +158,26 @@ const AllSAndC_SAndE = (props) => {
             </tr>
           </thead>
           <tbody>
-            {malls.map((mall) => (
-              <SorCItem mall={mall} MD={handleDelete} sEP={setEditPage} ME={EditMall} Mlength={malls.length} key={mall.id}/>
+            {allSAndC.map((SOrC) => (
+              <SorCItem
+                SOrC={SOrC}
+                handleDelete={handleDelete}
+                setEditPage={setEditPage}
+                key={SOrC.id}
+              />
             ))}
           </tbody>
         </table>
         {dialog.isLoading && (
           <Dialog
-            //Update
             nameProduct={dialog.nameProduct}
             onDialog={areUSureDelete}
             message={dialog.message}
           />
         )}
       </div>
-    )
+    );
   }
-}
+};
 
-export default AllSAndC_SAndE
+export default AllSAndC_SAndE;
