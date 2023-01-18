34c35ff97f7884424592c924c46d7cf91814a74d
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2023-01-18 14:06:30 +0200

    add dynamic ip for BE requastmants

--- my-app/src/helpers/helpers.js
+++ my-app/src/helpers/helpers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export var global = {
-  api: "http://localhost:3000/api/v1",
+  api: `http://${window.location.hostname}:3000/api/v1`,
   admin: { mall: {}, category: {}, tag: {}, item: {} },
 };
 
