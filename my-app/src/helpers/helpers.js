23521366318aa19167b19e6af67063233764aae4
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2023-01-20 19:57:10 +0200

    update env and helpers

--- my-app/src/helpers/helpers.js
+++ my-app/src/helpers/helpers.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
 export var global = {
-  api: `http://${process.env.REACT_APP_HOST}:3000/api/v1`,
-  testServer: process.env.REACT_APP_TEST,
+  api: `http://${
+    process.env.REACT_APP_IPV4 !== undefined
+      ? process.env.REACT_APP_IPV4
+      : "localhost"
+  }:3000/api/v1`,
+  testServer:
+    process.env.REACT_APP_TEST_SERVER !== undefined
+      ? process.env.REACT_APP_TEST_SERVER
+      : "false",
   admin: { mall: {}, category: {}, tag: {}, item: {} },
 };
 
