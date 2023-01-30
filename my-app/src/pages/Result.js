2a424f76e07dbea68709c274069375127a8c122c
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2023-01-30 12:10:27 +0200

    global.searchName is defalult search name in result page

--- my-app/src/pages/Result.js
+++ my-app/src/pages/Result.js
@@ -1,6 +1,9 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef } from 'react'
+import { global } from '../helpers/helpers'
 
 function Result() {
+  const searchNameRef = useRef(global.searchName)
+
   return (
     <Fragment>
       <div className="resPage">
@@ -35,6 +38,8 @@ function Result() {
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
               </svg>
               <input
+                defaultValue={searchNameRef.current}
+                ref={searchNameRef}
                 type="text"
                 name="name-search"
                 placeholder="Пошук за назвою"
