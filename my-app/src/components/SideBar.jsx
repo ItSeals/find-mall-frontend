98da9a00f95b7294f85696713946566e25348d97
Maksym Pushkar <110563638+MaksymPushkar@users.noreply.github.com>
2023-02-06 13:01:57 +0200

    fix sidebar styles

--- my-app/src/components/SideBar.jsx
+++ my-app/src/components/SideBar.jsx
@@ -7,7 +7,7 @@ const SideBar = (props) => {
   return (
     <div className="reactbootstrap">
     <div className="admin_sidebar" style={{display:'flex'}}>
-    <div className= 'col-2 min-vh-100 sidebar overflow-auto' style={{height: "100vh"}}>
+    <div className= 'min-vh-100 sidebar overflow-auto' style={{height: "100vh", minWidth: "294px", width: "294px"}}>
     <div className='row pt-6' style={{marginRight: "0"}}>
       <div className='col-8 admin_panel pt-2'>
       <img className='alt' src={require('../img/TonyStarkPhoto.PNG')} width="50" height="50" style={{borderRadius: "50%"}}></img> <span className='ms-2' >admin pannel</span>
@@ -17,28 +17,28 @@ const SideBar = (props) => {
         <il className='row'>
           <Link to={"/admin/malls"} className = 'col-12 ml px-2'>
             <div className='link'>
-            <i className ='bi bi-circle ms-5'></i> <span className= 'ms-2 d-none d-sm-inline'>Malls</span>
+            <i className ='bi bi-circle admin_sidebar-ms-5'></i> <span className= 'ms-2 d-none d-sm-inline'>Malls</span>
             </div>
           </Link>
         </il>
         <il className='row'>
           <Link to={"/admin/categories"} className = 'col-12 ml-1 px-2'>
             <div className='link'>
-            <i className ='bi bi-circle ms-5'></i> <span className= 'ms-2 d-none d-sm-inline'>Categories</span>
+            <i className ='bi bi-circle admin_sidebar-ms-5'></i> <span className= 'ms-2 d-none d-sm-inline'>Categories</span>
             </div>
           </Link>
         </il>
         <il className='row'>
           <Link to={"/admin/tags"} className = 'col-12 ml px-2' >
           <div className='link'>
-            <i className ='bi bi-circle ms-5'></i> <span className = 'ms-2 d-none d-sm-inline'>Tags</span>
+            <i className ='bi bi-circle admin_sidebar-ms-5'></i> <span className = 'ms-2 d-none d-sm-inline'>Tags</span>
             </div>
           </Link>
         </il>
         <il className='row'>
           <Link to={"/admin/items"} className = 'col-12 ml-1 px-2' >
           <div className='link'>
-            <i className ='bi bi-bag ms-5'></i> <span className = 'ms-2 d-none d-sm-inline'>All stores & cafes</span>
+            <i className ='bi bi-bag admin_sidebar-ms-5'></i> <span className = 'ms-2 d-none d-sm-inline'>All stores & cafes</span>
             </div>
           </Link>
         </il>
@@ -50,7 +50,7 @@ const SideBar = (props) => {
                 className={`col-12 ${index % 2 === 0 ? "ml" : "ml-1"} px-2`}
               >
                 <div className="link">
-                  <i className="bi bi-cart3 ms-5"></i>{" "}
+                  <i className="bi bi-cart3 admin_sidebar-ms-5"></i>{" "}
                   <span className="ms-2 d-none d-sm-inline">
                     {category.title}
                   </span>
@@ -61,7 +61,7 @@ const SideBar = (props) => {
         })}
       </ul>
     </div>
-    <div className='col-10'>
+    <div style={{width: "100%"}}>
       <Outlet />
     </div>
   </div></div>
